cb4f9490c4c15f31506bf0221f4f907026667d68
PolinaKuzmuk <polinka2082@gmail.com>
2023-01-08 20:21:14 +0100

    initialize project

---
+++ js/script.js
@@ -0,0 +1,444 @@
+const obj = JSON.parse(JSON.stringify(
+    [{
+        "season": 2022,
+        "status": "played",
+        "timeVenueUTC": "00:00:00",
+        "dateVenue": "2022-02-03",
+        "stadium": null,
+        "homeTeam": {
+            "name": "Al Shabab",
+            "officialName": "Al Shabab FC",
+            "slug": "al-shabab-fc",
+            "abbreviation": "SHA",
+            "teamCountryCode": "KSA",
+            "stagePosition": null
+        },
+        "awayTeam": {
+            "name": "Nasaf",
+            "officialName": "FC Nasaf",
+            "slug": "fc-nasaf-qarshi",
+            "abbreviation": "NAS",
+            "teamCountryCode": "UZB",
+            "stagePosition": null
+        },
+        "result": {
+            "homeGoals": 1,
+            "awayGoals": 2,
+            "winner": "Nasaf",
+            "message": null,
+            "goals": [],
+            "yellowCards": [],
+            "secondYellowCards": [],
+            "directRedCards": []
+        },
+        "stage": {
+            "id": "ROUND OF 16",
+            "name": "ROUND OF 16",
+            "ordering": 4
+        },
+        "group": null,
+        "originCompetitionId": "afc-champions-league",
+        "originCompetitionName": "AFC Champions League"
+    },
+    {
+        "season": 2022,
+        "status": "scheduled",
+        "timeVenueUTC": "16:00:00",
+        "dateVenue": "2023-02-03",
+        "stadium": null,
+        "homeTeam": {
+            "name": "Al Hilal",
+            "officialName": "Al Hilal Saudi FC",
+            "slug": "al-hilal-saudi-fc",
+            "abbreviation": "HIL",
+            "teamCountryCode": "KSA",
+            "stagePosition": null
+        },
+        "awayTeam": {
+            "name": "Shabab Al Ahli",
+            "officialName": "SHABAB AL AHLI DUBAI",
+            "slug": "shabab-al-ahli-club",
+            "abbreviation": "SAH",
+            "teamCountryCode": "UAE",
+            "stagePosition": null
+        },
+        "result": {
+            "homeGoals": 0,
+            "awayGoals": 0,
+            "winner": null,
+            "winnerId": null,
+            "message": null,
+            "goals": [],
+            "yellowCards": [],
+            "secondYellowCards": [],
+            "directRedCards": [],
+            "scoreByPeriods": null
+        },
+        "stage": {
+            "id": "ROUND OF 16",
+            "name": "ROUND OF 16",
+            "ordering": 4
+        },
+        "group": null,
+        "originCompetitionId": "afc-champions-league",
+        "originCompetitionName": "AFC Champions League"
+    },
+    {
+        "season": 2022,
+        "status": "scheduled",
+        "timeVenueUTC": "15:25:00",
+        "dateVenue": "2023-02-04",
+        "stadium": null,
+        "homeTeam": {
+            "name": "Al Duhail",
+            "officialName": "AL DUHAIL SC",
+            "slug": "al-duhail-sc",
+            "abbreviation": "DUH",
+            "teamCountryCode": "QAT",
+            "stagePosition": null
+        },
+        "awayTeam": {
+            "name": "Al Rayyan",
+            "officialName": "AL RAYYAN SC",
+            "slug": "al-rayyan-sc",
+            "abbreviation": "RYN",
+            "teamCountryCode": "QAT",
+            "stagePosition": null
+        },
+        "result": {
+            "homeGoals": 0,
+            "awayGoals": 0,
+            "winner": null,
+            "winnerId": null,
+            "message": null,
+            "goals": [],
+            "yellowCards": [],
+            "secondYellowCards": [],
+            "directRedCards": [],
+            "scoreByPeriods": null
+        },
+        "stage": {
+            "id": "ROUND OF 16",
+            "name": "ROUND OF 16",
+            "ordering": 4
+        },
+        "group": null,
+        "originCompetitionId": "afc-champions-league",
+        "originCompetitionName": "AFC Champions League"
+    },
+    {
+        "season": 2022,
+        "status": "scheduled",
+        "timeVenueUTC": "08:00:00",
+        "dateVenue": "2023-02-04",
+        "stadium": null,
+        "homeTeam": {
+            "name": "Al Faisaly",
+            "officialName": "Al Faisaly FC",
+            "slug": "al-faisaly-fc",
+            "abbreviation": "FAI",
+            "teamCountryCode": "KSA",
+            "stagePosition": null
+        },
+        "awayTeam": {
+            "name": "Foolad",
+            "officialName": "FOOLAD KHOUZESTAN FC",
+            "slug": "foolad-khuzestan-fc",
+            "abbreviation": "FLD",
+            "teamCountryCode": "IRN",
+            "stagePosition": null
+        },
+        "result": {
+            "homeGoals": 0,
+            "awayGoals": 0,
+            "winner": null,
+            "winnerId": null,
+            "message": null,
+            "goals": [],
+            "yellowCards": [],
+            "secondYellowCards": [],
+            "directRedCards": [],
+            "scoreByPeriods": null
+        },
+        "stage": {
+            "id": "ROUND OF 16",
+            "name": "ROUND OF 16",
+            "ordering": 4
+        },
+        "group": null,
+        "originCompetitionId": "afc-champions-league",
+        "originCompetitionName": "AFC Champions League"
+    },
+    {
+        "season": 2022,
+        "status": "scheduled",
+        "timeVenueUTC": "00:00:00",
+        "dateVenue": "2023-02-19",
+        "stadium": null,
+        "homeTeam": null,
+        "awayTeam": {
+            "name": "Urawa Reds",
+            "officialName": "Urawa Red Diamonds",
+            "slug": "urawa-red-diamonds",
+            "abbreviation": "RED",
+            "teamCountryCode": "JPN",
+            "stagePosition": null
+        },
+        "result": {
+            "homeGoals": 0,
+            "awayGoals": 0,
+            "winner": null,
+            "message": null,
+            "goals": [],
+            "yellowCards": [],
+            "secondYellowCards": [],
+            "directRedCards": [],
+            "scoreByPeriods": {
+                "firstHalf": {
+                    "home": null,
+                    "away": null
+                },
+                "secondHalf": {
+                    "home": null,
+                    "away": null
+                },
+                "thirdHalf": {
+                    "home": null,
+                    "away": null
+                },
+                "penalty": {
+                    "home": null,
+                    "away": null
+                },
+                "extraTime": {
+                    "home": null,
+                    "away": null
+                }
+            }
+        },
+        "stage": {
+            "id": "FINAL",
+            "name": "FINAL",
+            "ordering": 7
+        },
+        "group": null,
+        "originCompetitionId": "afc-champions-league",
+        "originCompetitionName": "AFC Champions League"
+    }]
+));
+
+const monthArr = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+const weekArr = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const today = new Date();
+const todaysYear = today.getFullYear();
+const todaysMonthIndex = today.getMonth();
+const todaysMonth = monthArr[todaysMonthIndex];
+const todaysDay = today.getDate();
+const daysInThisMonth = new Date(todaysYear, todaysMonthIndex + 1, 0).getDate();
+const nextBtn = document.getElementById('next');
+const prevBtn = document.getElementById('prev');
+const eventOverview = document.querySelector('.event-overview');
+const addEvent = document.querySelector('.add-event');
+const overlay = document.querySelector('.modal-overlay');
+const openAddEventBtn = document.getElementById('btn-1');
+const closeOverview = document.getElementsByClassName('modal-close')[0];
+const closeAddEvent = document.getElementsByClassName('modal-close')[1];
+
+function setCalendar() {
+    document.getElementsByClassName('year-item')[0].textContent = todaysYear;
+    document.getElementsByClassName('month-item')[0].textContent = todaysMonth;
+}
+
+function addDays(daysInMonth) {
+    for (i = 0; i < daysInMonth; i++) {
+        const newDay = document.createElement('p');
+        newDay.classList.add('day-item');
+        const textNode = document.createTextNode(i + 1);
+        newDay.appendChild(textNode);
+        document.getElementById('day').appendChild(newDay);
+    }
+}
+
+function highlightToday(year, month) {
+    if (year == document.querySelector('.year-item').textContent && month == document.querySelector('.month-item').textContent) {
+        document.getElementsByClassName('day-item')[today.getDate() - 1].classList.add('current-day');
+    }
+}
+
+function removeDays(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+};
+
+setCalendar();
+addDays(daysInThisMonth);
+highlightToday(todaysYear, todaysMonth);
+calendarActions();
+
+let calendarYear = document.getElementsByClassName('year-item')[0].textContent;
+let calendarMonth = document.getElementsByClassName('month-item')[0].textContent;
+let calendarMonthIndex = monthArr.indexOf(calendarMonth);
+
+function moveDays(calendarMonth) {
+    const firstDayOfMonth = new Date(calendarYear, monthArr.indexOf(calendarMonth), 1).getDay();
+    if (firstDayOfMonth === 1) {
+        document.getElementsByClassName('day-item')[0].classList.add('grid1');
+    } if (firstDayOfMonth === 2) {
+        document.getElementsByClassName('day-item')[0].classList.add('grid2');
+    } if (firstDayOfMonth === 3) {
+        document.getElementsByClassName('day-item')[0].classList.add('grid3');
+    } if (firstDayOfMonth === 4) {
+        document.getElementsByClassName('day-item')[0].classList.add('grid4');
+    } if (firstDayOfMonth === 5) {
+        document.getElementsByClassName('day-item')[0].classList.add('grid5');
+    } if (firstDayOfMonth === 6) {
+        document.getElementsByClassName('day-item')[0].classList.add('grid6');
+    }
+}
+
+nextBtn.addEventListener('click', function () {
+    removeDays(document.getElementById('day'));
+    calendarMonthIndex++;
+    if (calendarMonthIndex > 11) {
+        calendarMonthIndex = 0;
+        calendarYear++;
+    };
+    calendarMonth = monthArr[calendarMonthIndex];
+    document.getElementsByClassName('month-item')[0].textContent = calendarMonth;
+    document.getElementsByClassName('year-item')[0].textContent = calendarYear;
+    let daysInNextMonth = new Date(calendarYear, calendarMonthIndex + 1, 0).getDate();
+    addDays(daysInNextMonth);
+    moveDays(calendarMonth);
+    highlightToday(todaysYear, todaysMonth);
+    calendarActions();
+});
+
+prevBtn.addEventListener('click', function () {
+    removeDays(document.getElementById('day'));
+    calendarMonthIndex--;
+    if (calendarMonthIndex < 0) {
+        calendarMonthIndex = 11;
+        calendarYear--;
+    };
+    calendarMonth = monthArr[calendarMonthIndex];
+    document.getElementsByClassName('month-item')[0].textContent = calendarMonth;
+    document.getElementsByClassName('year-item')[0].textContent = calendarYear;
+    let daysInPrevMonth = new Date(calendarYear, calendarMonthIndex + 1, 0).getDate();
+    addDays(daysInPrevMonth);
+    moveDays(calendarMonth);
+    highlightToday(todaysYear, todaysMonth);
+    calendarActions();
+});
+
+function calendarActions() {
+    const year = document.querySelector('.year-item').textContent;
+    const month = document.querySelector('.month-item').textContent;
+    const dayList = document.querySelectorAll('.day-item');
+    const monthInd = monthArr.indexOf(month) + 1;
+
+    dayList.forEach(function (item) {
+        obj.forEach(function (i) {
+            if (('0' + item.textContent == i.dateVenue.slice(8) || item.textContent == i.dateVenue.slice(8)) && (i.dateVenue.slice(5, 7) == '0' + monthInd || i.dateVenue.slice(5, 7) == monthInd) && (i.dateVenue.slice(0, 4) == year)) {
+                const p = document.createElement('p');
+                p.classList.add('description');
+                item.setAttribute('id', item.textContent);
+                const textNode = document.createTextNode((i.homeTeam?.abbreviation || '') + ' - ' + (i.awayTeam.abbreviation || ''));
+                p.appendChild(textNode);
+                item.appendChild(p);
+
+                item.addEventListener('click', function () {
+                    eventOverview.classList.add('active');
+                    overlay.classList.add('active');
+                    document.querySelectorAll('.json-data').forEach(e => e.remove());
+                    const br = document.createElement('br');
+                    eventOverview.appendChild(br);
+                    for (j in i) {
+                        const p = document.createElement('p');
+                        p.classList.add('json-data');
+                        if (typeof (i[j]) !== "object") {
+                            if (i[j] != null) {
+                                p.innerText = i[j];
+                                if (j.split(/(?=[A-Z])/)) {
+                                    j = j.split(/(?=[A-Z])/).join(" ");
+                                }
+                                p.innerText = j + ': ' + p.innerText;
+                                eventOverview.appendChild(p);
+                            }
+                        } else if (typeof (i[j]) == "object") {
+                            for (k in i[j]) {
+                                if (typeof (i[j][k]) !== "object") {
+                                    if (i[j][k] != null) {
+                                        const p2 = document.createElement('p');
+                                        p2.classList.add('json-data');
+                                        p2.innerText = i[j][k];
+                                        if (k.split(/(?=[A-Z])/)) {
+                                            k = k.split(/(?=[A-Z])/).join(" ");
+                                        }
+                                        p2.innerText = k + ': ' + p2.innerText;
+                                        eventOverview.appendChild(p2);
+                                    }
+                                } else {
+                                    for (d in i[j][k]) {
+                                        if (typeof (i[j][k][d]) !== "object") {
+                                            if (i[j][k][d] != null) {
+                                                const p3 = document.createElement('p');
+                                                p3.classList.add('json-data');
+                                                p3.innerText = i[j][k][d];
+                                                if (d.split(/(?=[A-Z])/)) {
+                                                    d = d.split(/(?=[A-Z])/).join(" ");
+                                                }
+                                                p3.innerText = d + ': ' + p3.innerText;
+                                                eventOverview.appendChild(p3);
+                                            }
+
+                                        }
+                                    }
+                                }
+
+                            }
+
+                        }
+                    }
+                    openAddEventBtn.addEventListener('click', function () {
+                        const weekday = new Date(year, monthArr.indexOf(month), item.id);
+                        const weekdayName = weekArr[weekday.getDay()];
+                        overlay.classList.add('z-index-25');
+                        addEvent.classList.add('active');
+                        addEvent.querySelector('.weekday-event').textContent = weekdayName;
+                        addEvent.querySelector('.event-day').textContent = month + ' ' + item.id;
+                    });
+                    closeAddEvent.addEventListener('click', function () {
+                        addEvent.classList.remove('active');
+                        overlay.classList.remove('z-index-25');
+                    });
+                    closeOverview.addEventListener('click', function () {
+                        eventOverview.classList.remove('active');
+                        overlay.classList.remove('active');
+                    });
+                });
+            };
+        })
+    })
+    dayList.forEach(function (item) {
+        if (item.childElementCount === 0) {
+            let dayText = item.textContent;
+            let dateOfEvent = new Date(year, monthInd - 1, dayText);
+            let dayOfWeek = dateOfEvent.getDay();
+            item.addEventListener('click', function () {
+                overlay.classList.add('active');
+                addEvent.classList.add('active');
+                addEvent.querySelector('.weekday-event').textContent = weekArr[dayOfWeek];
+                addEvent.querySelector('.event-day').textContent = month + ' ' + dayText;
+                closeAddEvent.addEventListener('click', function () {
+                    addEvent.classList.remove('active');
+                    overlay.classList.remove('active');
+                });
+                closeAddEvent.addEventListener('click', function () {
+                    addEvent.classList.remove('active');
+                });
+            })
+
+        }
+    })
+}
